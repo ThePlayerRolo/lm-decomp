da85b48f187be00cd222af9641d587ba43f5842a
ThePlayerRolo <thesolis78243@gmail.com>
2026-06-10 18:03:19 -0400

    MapColl progress 2

--- include/Koga/MapCol.hpp
+++ include/Koga/MapCol.hpp
@@ -11,6 +11,12 @@ namespace Koga {
 
     class MapCol : public JORReflexible {
     public:
+
+        //TODO: Verify this since its confusing
+        struct unkSubStruct {
+            JGeometry::TVec3f _0[5];
+        };
+
         MapCol() : _4(nullptr), _8(0x8000), _C(-1), _10(-1) { }
         /* 0x08 */ virtual ~MapCol();
 
@@ -22,7 +28,7 @@ namespace Koga {
 
         JGeometry::TVec3f* fn_800BBBE0(JGeometry::TVec3f*);
         bool fn_800BBCA8(JGeometry::TVec3f*,  JGeometry::TVec3f&);
-        s32* fn_800BBD54(Vec*);
+        s32* fn_800BBD54(JGeometry::TVec3f&);
         void fn_800BC898(); // Is stubbed out
         void fn_800BC89C(s32);
         void fn_800BC8B4(s32);
@@ -31,7 +37,7 @@ namespace Koga {
         ToolData* getPolygonInfo();
 
         inline bool vecCheckto_4(JGeometry::TVec3f& param_1) {
-            if (param_1.x < 0.0f || _4[2].x <= param_1.x || param_1.y < 0.0f || _4[2].y <= param_1.y || param_1.z < 0.0f || _4[2].z <= param_1.z)
+            if (param_1.x < 0.0f || _4->_0[2].x <= param_1.x || param_1.y < 0.0f || _4->_0[2].y <= param_1.y || param_1.z < 0.0f || _4->_0[2].z <= param_1.z)
             {
                 return false;
             }
@@ -39,7 +45,7 @@ namespace Koga {
         }
 
     public:
-        /* 0x04 */  JGeometry::TVec3f* _4;
+        /* 0x04 */  unkSubStruct* _4;
         /* 0x08 */ u16 _8;
         /* 0x0C */ s32 _C;
         /* 0x10 */ s32 _10;
--- src/Koga/MapCol.cpp
+++ src/Koga/MapCol.cpp
@@ -13,7 +13,8 @@ s32 lbl_804D2D18 = 0;
 
 namespace Koga {
 
-    MapCol::~MapCol() { }
+    MapCol::~MapCol() {
+    }
 
     void MapCol::newMapCol() {
         sCurMapCol = new MapCol();
@@ -25,7 +26,7 @@ namespace Koga {
     }
 
     void MapCol::load(void* param_1, void* param_2) {
-        _4 = (JGeometry::TVec3f*)param_1;
+        _4 = (unkSubStruct*)param_1;
         if (fn_800B9F50("SoundPolygonInfo") != nullptr) {
             Koga::ToolData* soundPolygonInfo = fn_800B9F50("SoundPolygonInfo");
 
@@ -34,68 +35,72 @@ namespace Koga {
         }
     }
 
-    JGeometry::TVec3f* MapCol::fn_800BBBE0(JGeometry::TVec3f *param_1) {
+    // https://decomp.me/scratch/O6zkC
+    JGeometry::TVec3f* MapCol::fn_800BBBE0(JGeometry::TVec3f* param_1) {
         JGeometry::TVec3f stack_18;
         s32* val2;
 
         if (fn_800BBCA8(param_1, stack_18) == false) {
             val2 = &lbl_804D2D18;
         } else {
-            val2 = fn_800BBD54(&stack_18);
+            val2 = fn_800BBD54(stack_18);
         }
 
-//        s32 val = _4[12] + *val2;
-
-        return &_4[*val2];
-        //return _4(((_4[12] * 2) + *val2);
+        return &_4->_0[*val2];
     }
 
+    // https://decomp.me/scratch/cGlAK
     bool MapCol::fn_800BBCA8(JGeometry::TVec3f* param_1, JGeometry::TVec3f& param_2) {
-
-        PSVECSubtract(param_1, &_4[1], &param_2);
+        PSVECSubtract(param_1, &_4->_0[1], &param_2);
 
         return vecCheckto_4(param_2);
     }
 
+    // https://decomp.me/scratch/JJHFz
     f32 MapCol::fn_800BBE2C(JGeometry::TVec3f& param_1) {
         JGeometry::TVec3f stack_d8;
-        PSVECSubtract(&param_1, _4[1], &stack_d8);
+        PSVECSubtract(&param_1, &_4->_0[1], &stack_d8);
         bool vecCheck4 = vecCheckto_4(stack_d8);
 
-        if (!vecCheck4) return 32000.0f;
+        if (!vecCheck4)
+            return 32000.0f;
 
-        for (; stack_d8.y > _4[2].y; stack_d8.y += _4[2].y) {
+        for (; stack_d8.y > _4->_0[2].y; stack_d8.y += _4->_0[2].y) {
             bool bVar5 = false;
 
-            int iVar7 =(int)(_4[2].x / _4[0].x);
-            int iVar1 =(int)(stack_d8.z / _4[0].z);
-            int iVar2 =(int)(stack_d8.y / _4[0].y);
-            int iVar3 =(int)(_4[2].y / _4[0].y);
-            int iVar4 =(int)(stack_d8.x / _4[0].x);
-            iVar7++;
-
+            // int iVar7 =(int)(_4[2].x / _4[0].x);
+            // int iVar1 =(int)(stack_d8.z / _4[0].z);
+            // int iVar2 =(int)(stack_d8.y / _4[0].y);
+            // int iVar3 =(int)(_4[2].y / _4[0].y);
+            // int iVar4 =(int)(stack_d8.x / _4[0].x);
+            // iVar7++;
         }
     }
 
-    void MapCol::fn_800BC898() { }
+    void MapCol::fn_800BC898() {
+    }
 
     void MapCol::fn_800BC89C(s32 param_1) {
-        _8 |= 1 <<  param_1;
+        _8 |= 1 << param_1;
     }
 
     void MapCol::fn_800BC8B4(s32 param_1) {
-        _8 &= ~(1 <<  param_1);
+        _8 &= ~(1 << param_1);
     }
 
+    // https://decomp.me/scratch/KOzK8
     int MapCol::fn_800BC8CC(s16 param_1) {
-        if (param_1 < 0) return 0;
-        if (_10 < 0) return 0;
-        if (_C < 0) return 0;
+        if (param_1 < 0)
+            return 0;
+        if (_10 < 0)
+            return 0;
+        if (_C < 0)
+            return 0;
 
         Koga::ToolData* soundPolygonInfo = fn_800B9F50("SoundPolygonInfo");
         u32 p1 = param_1;
-        u32 firstVal = soundPolygonInfo->getUnsignedValue<u32>(_C, param_1);
-        u32 secondVal = soundPolygonInfo->getUnsignedValue<u32>(_10, param_1);
+        u32 firstVal = soundPolygonInfo->getUnsignedValue< u32 >(_C, param_1);
+        u32 secondVal = soundPolygonInfo->getUnsignedValue< u32 >(_10, param_1);
 
         return fn_8018D474(firstVal, secondVal, 0);
     }
@@ -103,4 +108,4 @@ namespace Koga {
     ToolData* MapCol::getPolygonInfo() {
         return fn_800B9F50("PolygonInfo");
     }
-};
+};  // namespace Koga
