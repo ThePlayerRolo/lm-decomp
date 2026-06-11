b159ee998561d088e7d6c1d6e524624ce5c51401
vlez <122755709+vlz1@users.noreply.github.com>
2026-06-11 14:40:13 -0500

    Remove vt_74 from EnAttackBase, which was probably just padding

--- include/Koga/EnAttackBase.hpp
+++ include/Koga/EnAttackBase.hpp
@@ -9,7 +9,6 @@ class EnAttackBase : public EnemyStrategy, public Koga::CharacterEventObserver {
 public:
     virtual ~EnAttackBase();
     virtual void* vt_70(); // Seems to be in a different TU than the destructor
-    virtual void vt_74() = 0; // This might just be padding, idk yet
 };
 
 #endif
