ddb145c613dc36be16d61774afa32e51e909592a
vlez <122755709+vlz1@users.noreply.github.com>
2026-06-11 13:06:28 -0500

    Might've missed a pure virtual function on EnAttackBase

--- include/Koga/EnAttackBase.hpp
+++ include/Koga/EnAttackBase.hpp
@@ -10,6 +10,7 @@ class EnAttackBase : public EnemyStrategy, public Koga::CharacterEventObserver {
 public:
     virtual ~EnAttackBase();
     virtual void* EnAttackBase_vt_70(); // Seems to be in a different TU than the destructor
+    virtual void EnAttackBase_vt_74() = 0; // This might just be padding, idk yet
 };
 
 #endif
