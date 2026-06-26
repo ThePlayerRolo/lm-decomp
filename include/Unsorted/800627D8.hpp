ac4efee47e70e5eefd5fbfa5e2fc3651c500cdca
SomeJakeGuy <somejakeguy@gmail.com>
2026-06-25 21:32:22 -0400

    Added setLuigiAppearPoint as well.

--- include/Unsorted/800627D8.hpp
+++ include/Unsorted/800627D8.hpp
@@ -3,6 +3,7 @@
 
 #include <types.h>
 
+void setLuigiAppearPoint(u32);
 u32 getLuigiAppearPoint(); //fn_800627D8() I assume this is probably the one set from LM's custom events but not sure.
 
 static u32 sGlobalLuigiAppearPoint; //lbl_804D872C
--- src/Unsorted/800627D8.cpp
+++ src/Unsorted/800627D8.cpp
@@ -1,5 +1,9 @@
 #include "Unsorted/800627D8.hpp"
 
+void setLuigiAppearPoint(u32 apperPoint) {
+    sGlobalLuigiAppearPoint = apperPoint;
+}
+
 u32 getLuigiAppearPoint() {
     u32 temp = sGlobalLuigiAppearPoint;
     sGlobalLuigiAppearPoint = 0;
